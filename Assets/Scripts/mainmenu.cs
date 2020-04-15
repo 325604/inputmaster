adb8795acb297ffee4f4a88e83294ceaf10b2f6e
Rick Letschert <625604@student.mboutrecht.nl>
2020-04-15 13:41:57 +0200

    no message

---
+++ Assets/Scripts/mainmenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class mainmenu : MonoBehaviour
+{
+//dit verteld dat als je op play drukt dat de game ook echt gaat beginnen 
+    public void playgame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+//dit verteld dat de game word gestopt  en dat wordt exstra aan gegeven met debug.log
+    public void quitgame()
+    {
+        Debug.Log("quit");
+        Application.Quit();
+    }
+    
+    
+}
